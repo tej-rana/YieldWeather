3d1de9ece1a88c503a3b1648b8224526113b41c6
Tejaswi <Tejaswi@TEJASWI-PC>
2016-05-17 21:37:05 +1000

    Add project files.

---
+++ YieldWeather/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="YieldWeather.MvcApplication" Language="C#" %>
---
+++ YieldWeather/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(YieldWeather.Startup))]
+namespace YieldWeather
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
